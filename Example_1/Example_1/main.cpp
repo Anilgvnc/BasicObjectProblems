4033485b3718d1fa98de62c0381f4b04230b6166
Anilgvnc <anilnehir@hotmail.com>
2022-05-04 16:05:29 +0300

    Example 1: Create an object and access the attributes

--- Example_1/Example_1/main.cpp
+++ Example_1/Example_1/main.cpp
@@ -4,25 +4,15 @@ using namespace std;
 
 class myClass {
 public:
-	void setsalary(int s) {
-		salary = s;//Access a private attribute
-	}
-	int getsalary();//Define a method Exp2
-private:
-	int salary;
+	int myInt;
+	string myString;
 };
-
-int myClass::getsalary() {
-	return salary;
-}
 
 int main() {
 	myClass myObj;//create an object of myClass
-	//Exp2 & 3 define a method and access a private attribute
-	int x;
-	cout << "Set salary: ";
-	cin >> x;
-	myObj.setsalary(x);
-	cout << myObj.getsalary() << endl;
+	//Exp1: Create an object and access the attributes
+	myObj.myInt = 50;
+	myObj.myString = "Hello World.";
+	cout << myObj.myInt << "\n" << myObj.myString << endl;
 	return 0;
 }
